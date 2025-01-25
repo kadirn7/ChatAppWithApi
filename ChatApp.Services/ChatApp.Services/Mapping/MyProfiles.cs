a3138081c5ef1e034ff04d22e70b3a3f84eb8fa0
Kadir <Kadir@DESKTOP-P6D4VNP>
2025-01-25 11:55:00 +0300

    Message and Group Controller added

--- ChatApp.Services/ChatApp.Services/Mapping/MyProfiles.cs
+++ ChatApp.Services/ChatApp.Services/Mapping/MyProfiles.cs
@@ -5,16 +5,28 @@ using System.Text;
 using System.Threading.Tasks;
 using AutoMapper;
 using ChatApp.Data.Entities;
+using ChatApp.Services.Models.Group;
+using ChatApp.Services.Models.Message;
 using ChatApp.Services.Models.User;
+
 namespace ChatApp.Services.Mapping
 {
-    public class MyProfiles:Profile
+    public class MyProfiles: Profile
     {
         public MyProfiles()
         {
             CreateMap<User, UserCreateModel>().ReverseMap();
             CreateMap<User, UserModel>().ReverseMap();
-            CreateMap<User,UserUpdateModel>().ReverseMap();
+            CreateMap<User, UserUpdateModel>().ReverseMap();
+
+            CreateMap<Message, MessageModel>().ReverseMap();
+            CreateMap<Message, MessageCreateModel>().ReverseMap();
+            CreateMap<Message, MessageUpdateModel>().ReverseMap();
+
+            CreateMap<Group, GroupModel>().ReverseMap();
+            CreateMap<Group, GroupCreateModel>().ReverseMap();
+            CreateMap<Group, GroupUpdateModel>().ReverseMap();
+
         }
     }
 }
---
+++ ChatApp/ChatApp/Controllers/GroupController.cs
@@ -0,0 +1,78 @@
+﻿using AutoMapper;
+using ChatApp.Data.Entities;
+using ChatApp.Data.Models;
+using ChatApp.Models;
+using ChatApp.Services.Models.Group;
+using ChatApp.Services.Models.Message;
+using ChatApp.Services.Services.GroupService;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChatApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GroupController : ControllerBase
+    {
+        private readonly GroupService _groupService;
+        private readonly IMapper _mapper;
+
+        public GroupController(IMapper mapper, GroupService groupService)
+        {
+            _mapper = mapper;
+            _groupService = groupService;
+        }
+        [HttpGet]
+        public async Task<ReturnModel> GetMessage([FromQuery] PaginationModel paginationModel)
+        {
+            var messages = await _groupService.ListAllAsync(paginationModel);
+            return new ReturnModel
+            {
+                Success = true,
+                Message = "Success",
+                Data = _mapper.Map<List<MessageModel>>(messages),
+                StatusCode = 200,
+                TotalCount = await _groupService.CountAsync()
+            };
+        }
+        [HttpGet("{id}")]
+        public async Task<ReturnModel> GetMessageById(int id)
+        {
+            var message = await _groupService.GetByIdAsync(id);
+            return new ReturnModel
+            {
+                Success = true,
+                Message = "Success",
+                Data = _mapper.Map<MessageModel>(message),
+                StatusCode = 200
+            };
+        }
+        [HttpPost]
+        public async Task<ReturnModel> CreateGroup([FromBody] GroupCreateModel groupCreateModel)
+        {
+            var group = _mapper.Map<Group>(groupCreateModel);
+            var Result = await _groupService.AddAsync(group);
+            return new ReturnModel
+            {
+                Success = true,
+                Message = "Success",
+                Data = _mapper.Map<MessageModel>(Result),
+                StatusCode = 201
+            };
+        }
+        [HttpPut]
+        public async Task<ReturnModel> UpdateGroup([FromBody] GroupUpdateModel groupUpdateModel)
+        {
+            var group = _mapper.Map<Group>(groupUpdateModel);
+            var Result = await _groupService.UpdateAsync(group);
+            return new ReturnModel
+            {
+                Success = true,
+                Message = "Success",
+                Data = _mapper.Map<MessageModel>(Result),
+                StatusCode = 200
+            };
+        }
+
+    }
+}
